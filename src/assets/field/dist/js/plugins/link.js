59633258136ff6110bf6032046c68f9c44b06b78
Bert Oost <bert@oostdesign.com>
2018-01-21 17:58:39 +0100

    Fixed editor uploading files via Craft modal & fixed editing link
    Since links could be assets or entries, figure this out before launching the modal

--- src/assets/field/dist/js/plugins/link.js
+++ src/assets/field/dist/js/plugins/link.js
@@ -38,18 +38,43 @@ $(function() {
 
     $.FE.RegisterCommand('linkEdit', $.extend($.FE.COMMANDS['linkEdit'], {
         callback: function (cmd, val) {
-            var $currentLink = this.link.get();
+            var linkIsEntry = true,
+                disabledElementIds = [],
+                $currentLink = $(this.link.get());
 
-            var modal = Craft.createElementSelectorModal(this.opts.craftLinkElementType, {
-                storageKey: (this.opts.craftLinkStorageKey || 'FroalaInput.LinkTo.' + this.opts.craftLinkElementType),
-                sources: this.opts.craftLinkSources,
-                criteria: $.extend({ siteId: this.opts.craftLinkElementSiteId }, this.opts.craftLinkCriteria),
+            if ($currentLink.attr('href').indexOf('#') !== -1) {
+
+                // otherwise check the src url containing '#asset:{id}[:{transform}]'
+                var hashValue = $currentLink.attr('href').substr(($currentLink.attr('href').indexOf('#') + 1));
+                    hashValue = decodeURIComponent(hashValue);
+
+                if (hashValue.indexOf(':') !== -1) {
+                    disabledElementIds.push(hashValue.split(':')[1]);
+
+                    var linkKind = hashValue.split(':')[0];
+                    switch (linkKind) {
+                        case 'asset':
+                            linkIsEntry = false;
+                            break;
+                    }
+                }
+            }
+
+            var modalElementType = (linkIsEntry ? this.opts.craftLinkElementType : this.opts.craftAssetElementType),
+                modalCriteria = (linkIsEntry ? this.opts.craftLinkCriteria : this.opts.craftFileCriteria),
+                modalStorageKey = (linkIsEntry ? this.opts.craftLinkStorageKey : this.opts.craftFileStorageKey),
+                modalRefHandle = (linkIsEntry ? this.opts.craftLinkElementRefHandle : this.opts.craftAssetElementRefHandle);
+
+            var modal = Craft.createElementSelectorModal(modalElementType, {
+                storageKey: (modalStorageKey || 'FroalaInput.LinkTo.' + modalElementType),
+                sources: (linkIsEntry ? this.opts.craftLinkSources : this.opts.craftFileSources),
+                criteria: $.extend({ siteId: this.opts.craftElementSiteId }, modalCriteria),
+                disabledElementIds: disabledElementIds,
                 onSelect: $.proxy(function(elements) {
                     if (elements.length) {
                         var element = elements[0],
-                            url = element.url + '#' + this.opts.craftLinkElementRefHandle + ':' + element.id;
+                            url = element.url + '#' + modalRefHandle + ':' + element.id;
 
-                        $currentLink = $($currentLink);
                         $currentLink.attr('href', url);
 
                         return true;
@@ -59,4 +84,5 @@ $(function() {
             });
         }
     }));
+
 });
