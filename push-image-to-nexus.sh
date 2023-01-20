26b8e735ba2948e782e951731cb072460b88e7eb
Gangadhar <gangadhar.k@CELSYS217-BLR.CELSYSWTC.IN>
2023-01-20 07:24:57 +0530

    deploy-yes

--- push-image-to-nexus.sh
+++ push-image-to-nexus.sh
@@ -3,23 +3,6 @@
 #
 #  Build & upload docker image to Nexus
 #
-#
-#  Validate that this is a valid branch for CI/CD
-#
-if [ ${TRAVIS_PULL_REQUEST} != "false" ];  then echo "Not publishing a pull request !!!" && exit 0; fi
-
-export BRANCH_NAME=`echo "${TRAVIS_BRANCH}" | tr '[:upper:]' '[:lower:]'`
-case "${BRANCH_NAME}" in
-        dev*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD" ;;
-	       ao-dev*)echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD"  ;;
-        qa*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD"  ;;
-	       qe*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD"  ;;
-	       rc*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD"  ;;
-	       release-master*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD"  ;;
-        ft*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI" ;;
-        bf*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI" ;;
-        *) echo "Not a valid branch name for CI/CD" && exit -1;;
-esac
 
 echo $TRAVIS_BRANCH
 echo ${DEPLOYMENT_SERVER}
@@ -37,17 +20,14 @@ echo "Package name : ${PACKAGE_NAME}"
 #
 # Update package.json with wright froala editor name & version
 #
-jq --arg froalaeditor "file:${PACKAGE_NAME}-${PACKAGE_VERSION}.tgz" '.dependencies["froala-editor"] |= $froalaeditor' package.json  > new.file && cat new.file > package.json && rm -f new.file
+#jq --arg froalaeditor "file:${PACKAGE_NAME}-${PACKAGE_VERSION}.tgz" '.dependencies["froala-editor"] |= $froalaeditor' package.json  > new.file && cat new.file > package.json && rm -f new.file
 echo "verify package"
-cat package.json
+#cat package.json
 
-docker build --no cache -t  ${IMAGE_NAME}:${SHORT_COMMIT} --build-arg PackageName=${PACKAGE_NAME} --build-arg PackageVersion=${PACKAGE_VERSION} --build-arg NexusUser=${NEXUS_USER} --build-arg NexusPassword=${NEXUS_USER_PWD} .
+docker build -t  ${IMAGE_NAME}:${SHORT_COMMIT} --build-arg PackageName=${PACKAGE_NAME} --build-arg PackageVersion=${PACKAGE_VERSION} --build-arg NexusUser=${NEXUS_USER} --build-arg NexusPassword=${NEXUS_USER_PWD} .
 sleep 3
 docker image ls 
 echo "uploading to nexus" ${PACKAGE_NAME}
-#
-#  don't upload for new PR
-#
 
 docker login -u ${NEXUS_USER} -p ${NEXUS_USER_PWD} ${NEXUS_CR_TOOLS_URL}
 docker tag  ${IMAGE_NAME}:${SHORT_COMMIT} ${NEXUS_CR_TOOLS_URL}/froala-${IMAGE_NAME}:${PACKAGE_VERSION}
